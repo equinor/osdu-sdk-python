5c523347df02943d7af7af18586d06c23aa9b9f7
Mark Hewitt <mhew@equinor.com>
2022-02-15 15:00:12 +0100

    Feature/python3.10 (#4)
    * Test in python 3.10
    
    * Updated build workflow for python  3.10
    
    * Update build.yml
    
    * Add python 3.10

--- setup.py
+++ setup.py
@@ -24,40 +24,39 @@ def read(fname):
     return open(join(osdu_path, fname)).read()
 
 
-version_file = read(os.path.join('src', 'osdu', '__init__.py'))
-__VERSION__ = re.search(r'^__VERSION__\s*=\s*[\'"]([^\'"]*)[\'"]',
-                        version_file, re.MULTILINE).group(1)
+version_file = read(os.path.join("src", "osdu", "__init__.py"))
+__VERSION__ = re.search(
+    r'^__VERSION__\s*=\s*[\'"]([^\'"]*)[\'"]', version_file, re.MULTILINE
+).group(1)
 
 setup(
-    name='osdu-sdk',
+    name="osdu-sdk",
     version=__VERSION__,
-    description='OSDU SDK for Python',
-    long_description=read('README.rst'),
-    url='https://github.com/equinor/osdu-sdk-python',
-    author='Equinor ASA',
-    author_email='mhew@equinor.com',
-    license='MIT',
+    description="OSDU SDK for Python",
+    long_description=read("README.rst"),
+    url="https://github.com/equinor/osdu-sdk-python",
+    author="Equinor ASA",
+    author_email="mhew@equinor.com",
+    license="MIT",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        'Environment :: Console',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9'
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
-    keywords='osdu sdk python',
-    python_requires='>=3.8',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    keywords="osdu sdk python",
+    python_requires=">=3.8",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
     include_package_data=True,
-    install_requires=[
-        'requests',
-        'msal'
-    ],
+    install_requires=["requests", "msal"],
     project_urls={
-        'Issue Tracker': 'https://github.com/equinor/osdu-sdk-python/issues',
-    }
+        "Issue Tracker": "https://github.com/equinor/osdu-sdk-python/issues",
+    },
 )
